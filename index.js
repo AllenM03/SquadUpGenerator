62b141d63f7cb52f91de5e0653a05f87d4b05e58
Allen <almcbroombey@hotmail.com>
2022-05-13 04:57:09 -0400

    added class functions

---
+++ index.js
@@ -0,0 +1,5 @@
+const Enginer = require("/lib/engineer");
+const Intern = require("/lib/intern");
+const manager = require("/lib/manager");
+const inquirer = require("inquirer");
+const fs = require("fs");
